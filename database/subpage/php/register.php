c69858c22b7238f1eacd049952f4c9709b213f8a
Katze <71961855+0xKatze@users.noreply.github.com>
2022-06-07 09:51:03 +0800

    fix: account system

--- database/subpage/php/register.php
+++ database/subpage/php/register.php
@@ -25,17 +25,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     echo "<script type='text/javascript'>alert('username = $username, password = $password, mail = $mail');</script>";
 
-    //檢查帳號是否重複
-    /*
-    $check = "SELECT * FROM user WHERE username='" . $username . "'";
+    $check = "SELECT * FROM user WHERE userAccount='" . $username . "'";
     if (mysqli_num_rows(mysqli_query($conn, $check)) == 0) {
-        $sql = "INSERT INTO user (id, username, password, mail)
-            VALUES(NULL,'" . $username . "','" . $password . "','" . $password . "')";
+        $sql = "INSERT INTO user (userId, userAccount, userPassword, email, storeUrl)
+            VALUES(NULL,'" . $username . "','" . $password . "','" . $mail . "','www.temp.com')";
 
         if (mysqli_query($conn, $sql)) {
             echo "註冊成功!3秒後將自動跳轉頁面<br>";
             echo "<a href='index.php'>未成功跳轉頁面請點擊此</a>";
-            header("refresh:32;url=index.php");
+            header("refresh:32;url=../index.php");
             exit;
         } else {
             echo "Error creating table: " . mysqli_error($conn);
@@ -44,12 +42,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "該帳號已有人使用!<br>3秒後將自動跳轉頁面<br>";
         echo "<a href='register.html'>未成功跳轉頁面請點擊此</a>";
         header('HTTP/1.0 302 Found');
-        //header("refresh:3;url=register.html",true);
+        header("refresh:3;url=../register.html",true);
         exit;
     }
-    */
+    
 }
-//mysqli_close($conn);
+mysqli_close($conn);
 
 /*
 function function_alert($message) {
